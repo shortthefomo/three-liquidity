1827b5894a6a26ce6febb936aed13222665b9086
Lathan <jucallme@gmail.com>
2025-09-18 12:22:18 -0300

    add narrow

--- src/liquidity.js
+++ src/liquidity.js
@@ -61,8 +61,8 @@ class main {
 
             const asset_narrow = {}
             for (const [key, value] of Object.entries(pairDetails)) {
-              if (value.asset1.currency !== req.query.currency || value.asset2.currency !== req.query.currency) { continue }
-              if (value.asset1.issuer !== req.query.issuer || value.asset2.issuer !== req.query.issuer) { continue }
+              if (value.asset1.currency !== req.query.currency && value.asset2.currency !== req.query.currency) { continue }
+              if (value.asset1.issuer !== req.query.issuer && value.asset2.issuer !== req.query.issuer) { continue }
               asset_narrow[key] = value
             }
 
